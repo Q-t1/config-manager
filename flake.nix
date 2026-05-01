15443df1a00d4961cdff212d22ef483f58a3c83d
Qt1 <quentin.roccia@gmail.com>
2026-05-01 23:06:50 +0200

    Fixing stuff

--- flake.nix
+++ flake.nix
@@ -7,26 +7,44 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self, nixpkgs, home-manager, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        username = "qt1";  # your user
-      in
-      {
-        homeConfigurations."${username}@${system}" =
-          home-manager.lib.homeManagerConfiguration {
-            inherit pkgs username;
-            homeDirectory = "/home/${username}";
-            stateVersion = "25.11";
-
-            modules = [
-              ./common/default.nix
-            ];
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      username = "qt1";
+      mkHome = { profile, system }:
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.${system};
+          extraSpecialArgs = {
+            inherit profile system username;
           };
-      });
+          modules = [
+            ./common/default.nix
+          ];
+
+          # Non‑module-level items (would be overridden by modules anyway)
+          config.home.username = username;
+          config.home.homeDirectory = "/home/${username}";
+          config.home.stateVersion = "25.11";
+          config.programs.home-manager.enable = true;
+        };
+    in
+    {
+      homeConfigurations = {
+        orbstack = mkHome {
+          profile = "orbstack";
+          system = "aarch64-linux";
+        };
+
+        foundation = mkHome {
+          profile = "foundation";
+          system = "x86_64-linux";
+        };
+
+        wsl = mkHome {
+          profile = "wsl";
+          system = "x86_64-linux";
+        };
+      };
+    };
 }
